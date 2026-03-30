4d039c4db1e36a49149fddf0fc7465597b656b5c
A. Can Ersoy <ahmetcanersoy@gmail.com>
2026-03-31 00:39:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const cards = document.querySelectorAll('.card');
+const projectTrigger = document.getElementById('project-trigger');
+const mainImage = document.getElementById('main-image');
+const tealBox = document.getElementById('shape-teal');
+const mustardBox = document.getElementById('shape-mustard');
 
-cards.forEach(card => {
-    card.addEventListener('mouseenter', () => {
-        removeActiveClasses();
-        card.classList.add('active');
-    });
+// Hover Başladığında
+projectTrigger.addEventListener('mouseenter', () => {
+    // Yeni görsele geçiş (Örn: Purenest)
+    mainImage.src = 'purenest.webp'; 
+    
+    // Renkleri değiştir
+    tealBox.style.background = '#F7CAC9'; // Purenest pembesi
+    mustardBox.style.background = '#E0E7E9';
 });
 
-function removeActiveClasses() {
-    cards.forEach(card => {
-        card.classList.remove('active');
-    });
-}
+// Hover Bittiğinde (Orijinal Haline Dönüş)
+projectTrigger.addEventListener('mouseleave', () => {
+    mainImage.src = 'herceptin.webp';
+    tealBox.style.background = '#0A6E82';
+    mustardBox.style.background = '#C3870F';
+});
